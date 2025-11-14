828739ec093e3653dbcae4c76df3340b3a9c6e7a
murphy-afk <bardarenoemi@gmail.com>
2025-11-14 14:26:24 +0100

    feat: email list output

--- js/script.js
+++ js/script.js
@@ -12,9 +12,12 @@ const emailListContainer = document.querySelector(".email-list-container");
 
 // const emailListItem = `<li>${email}</li>`;
 
+for (let i = 0; i < 10; i++) {
 axios
   .get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((resp) => {
-    console.log(resp.data.response);
-    
+    const email = resp.data.response;
+    const emailListItem = `<li>${email}</li>`;
+    emailListContainer.innerHTML += emailListItem;
   });
+};
